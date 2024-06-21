992f395231c8b2c6db146481bd3a9e7b5090f76a
teddy8587 <teddy.barieraud@gmail.com>
2024-06-21 20:28:16 +0200

    feat modal2 for add photos

--- modal.js
+++ modal.js
@@ -92,28 +92,61 @@ export function modal2() {
     const modalContent = document.createElement("div");
     modalContent.classList.add("modal-content");
 
-    const titre = document.createElement("h1");
-    const bar = document.createElement("hr");
-    const button = document.createElement("button");
     const returnButton = document.createElement("i");
     const closeButton = document.createElement("i");
+    const titre = document.createElement("h1");
+    const ajoutPhoto = document.createElement("div");
+    const formContainer1 = document.createElement("div");
+    const label1 = document.createElement("p");
+    const labelContainer1 = document.createElement("input");
+    const formContainer2 = document.createElement("div");
+    const label2 = document.createElement("p");
+    const labelContainer2 = document.createElement("select");
+    const bar = document.createElement("hr");
+    const button = document.createElement("button");
+    const image = document.createElement("i");
+    const button2 = document.createElement("button");
+    const fileSize = document.createElement("p");
 
     // Définir le contenu
+    returnButton.classList.add("modal-return", "fa-solid", "fa-arrow-left");
+    closeButton.classList.add("modal-close", "fa-solid", "fa-xmark");
     titre.textContent = "Ajout photo";
     titre.classList.add("title");
+    ajoutPhoto.classList.add("ajoutPhoto");
+    image.classList.add("fa-regular", "fa-image", "image")
+    button2.textContent = "+ Ajouter photo";
+    button2.classList.add("button2");
+    fileSize.textContent = "jpg,png : 4mo max";
+    fileSize.style.fontSize = "12px";
+    formContainer1.classList.add("formContainer");
+    label1.textContent = "Titre";
+    labelContainer1.classList.add("input");
+    formContainer2.classList.add("formContainer");
+    label2.textContent = "Categorie";
+    labelContainer2.classList.add("input");
     bar.classList.add("bar");
     button.textContent = "Valider";
     button.classList.add("button", "btnValidate");
-    returnButton.classList.add("modal-return", "fa-solid", "fa-arrow-left");
-    closeButton.classList.add("modal-close", "fa-solid", "fa-xmark");
 
     // Ajouter les éléments au modal
     modal.appendChild(modalContent);
+    modalContent.appendChild(returnButton);
+    modalContent.appendChild(closeButton);
     modalContent.appendChild(titre);
+    modalContent.appendChild(ajoutPhoto);
+    modalContent.appendChild(formContainer1);
+    formContainer1.appendChild(label1);
+    formContainer1.appendChild(labelContainer1);
+    modalContent.appendChild(formContainer2);
+    formContainer2.appendChild(label2);
+    formContainer2.appendChild(labelContainer2);
     modalContent.appendChild(bar);
     modalContent.appendChild(button);
-    modalContent.appendChild(closeButton);
-    modalContent.appendChild(returnButton);
+    ajoutPhoto.appendChild(image);
+    ajoutPhoto.appendChild(button2);
+    ajoutPhoto.appendChild(fileSize);
+
 
     // Ajouter le modal au body de la page
     document.body.appendChild(modal);
